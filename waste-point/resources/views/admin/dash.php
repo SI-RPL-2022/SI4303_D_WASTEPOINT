b7e95855466425d5dd05aa88e858626a8461d5ac
Hanifka <81150342+Hanifka@users.noreply.github.com>
2022-04-04 22:01:15 +0700

    Halaman Dashboard admin

---
+++ waste-point/resources/views/admin/dash.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome Admin</h1>
+    <form action ="{{ Route('logout') }}" method="POST">
+        @csrf
+        <button type="submit" class=""><i class="bi bi-box-arrow-right me-2"></i>Logout</button>
+    </form>
+</body>
+</html>
