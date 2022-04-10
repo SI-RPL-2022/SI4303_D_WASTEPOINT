a4f5ce708fad989382c5a144428a382bacbd2c4d
rafifaqilakhanaspie <rafifak@student.telkomuniversity.ac.id>
2022-04-10 13:30:08 +0700

    update progress ref #4

--- waste-point/app/Http/Controllers/Admin/PengelolaanSampahController.php
+++ waste-point/app/Http/Controllers/Admin/PengelolaanSampahController.php
@@ -5,11 +5,17 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Waste;
 
 class PengelolaanSampahController extends Controller
 {
     public function index()
     {
-        return view('admin.penukaran_sampah');
+        $number = 1;
+        $wastes = Waste::all();
+        return view('admin.penukaran_sampah', [
+            'wastes' => $wastes,
+            'number' => $number
+        ]);
     }
 }
