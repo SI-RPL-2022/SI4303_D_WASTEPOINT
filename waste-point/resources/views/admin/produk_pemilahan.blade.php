d206b6c7ed5cdbeff440aa1d14e7c12719a89011
Hanifka <81150342+Hanifka@users.noreply.github.com>
2022-04-15 13:38:31 +0700

    ralat sedikit

--- waste-point/resources/views/admin/produk_pemilahan.blade.php
+++ waste-point/resources/views/admin/produk_pemilahan.blade.php
@@ -73,28 +73,5 @@
         @endif
     </div>
 
-    {{-- <!-- delete modal -->
-    <div class="modal modal-delete fade" id="deleteModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title fw-bold" id="deleteModalLabel">Hapus Data Produk</h5>
-                    <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    Anda yakin ingin menghapus permanen data produk?
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
-                    @foreach ($products as $product)
-                        <form action="data-produk-pemilahan/delete/{{ $product->id }}" method="POST">
-                             @csrf
-                            <button type="submit" class="btn btn-danger">Hapus</button>
-                        </form>
-                    @endforeach
-                </div>
-            </div>
-        </div> --}}
-    {{-- </div>
-     --}}
+
 @endsection
